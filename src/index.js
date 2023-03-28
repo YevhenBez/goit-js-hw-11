87c8b4061a29f6c39003352b95a2a9142905aab7
YevhenBez <byxgalter@bigmir.net>
2023-03-28 13:30:05 +0300

    Этим куском кода я сделал запрос  на публичный API сервис Pixabay     и проверил получение данных по данному запросу. Модуль 11
    Этим куском кода я сделал запрос  на публичный API сервис Pixabay    (который расположен по пути     https://pixabay.com/api/docs/       )   при помощи       const fetchPhotos = name => fetch(`https://pixabay.com/api/?key=34783600-4c4882faf47dfa22b7423406f&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`)       и там же сразу написал код в случае возврата ошибки     (     .then(response => {
            if (!response.ok) {
                throw new Error(response.status);
            }
            return response.json();
        });            )    и проверил получение данных по данному запросу при помощи    (      fetchPhotos(seekedPhoto).then(response => {
            console.log(response);
        }).catch(error => { console.log(error); })         )   . Модуль 11

--- src/index.js
+++ src/index.js
@@ -8,11 +8,22 @@ const btnSearch = searchForm.lastElementChild;
 
 const inputSearch = searchForm.firstElementChild;
 
+const fetchPhotos = name => fetch(`https://pixabay.com/api/?key=34783600-4c4882faf47dfa22b7423406f&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    });
+
 const handleFormSubmit = event => {
     event.preventDefault();
 
     const seekedPhoto = inputSearch.value;
     
+    fetchPhotos(seekedPhoto).then(response => {
+        console.log(response);
+    }).catch(error => { console.log(error); })
 }
 
 searchForm.addEventListener('submit', handleFormSubmit);
