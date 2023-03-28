48a7bf3aa4bfcf74d64fd51b14ad6afc8d47f7f1
YevhenBez <byxgalter@bigmir.net>
2023-03-29 00:40:45 +0300

    Этим куском кода я нашёл <div class="gallery"></div>     при помощи       const gallery           Написал функцию        function renderPhotos      для создания разметки под карточки фото    и вставил данную разметку динамически в HTML    при помощи      gallery.insertAdjacentHTML('beforeend', renderPhotos(photos))         Модуль 11
    Этим куском кода я нашёл <div class="gallery"></div>     при помощи       const gallery           Написал функцию        function renderPhotos      для создания разметки под карточки фото (при этом обрати внимание, что в данной функции я распылил св-во    hits    потому что именно в данном св-ве возвращается массив с необходимыми данными и картинками)       и вставил данную разметку динамически в HTML    при помощи      gallery.insertAdjacentHTML('beforeend', renderPhotos(photos))         Модуль 11

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const btnSearch = searchForm.lastElementChild;
 
 const inputSearch = searchForm.firstElementChild;
 
+const gallery = document.querySelector(".gallery");
+
 const fetchPhotos = name => fetch(`https://pixabay.com/api/?key=34783600-4c4882faf47dfa22b7423406f&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`)
     .then(response => {
         if (!response.ok) {
@@ -24,16 +26,45 @@ const handleFormSubmit = async (event) => {
     
         const responsePhotos = await fetchPhotos(seekedPhoto);
         const photos = await responsePhotos;
-        console.log(photos.hits);
+        console.log(photos);
 
         if (photos.hits.length === 0) {
         emptyArray()
         }
+
+        gallery.insertAdjacentHTML('beforeend', renderPhotos(photos))
     
     } catch(error) { console.log(error); }
 }
 
 searchForm.addEventListener('submit', handleFormSubmit);
+
+function renderPhotos({ hits }) {
+    const markup = hits
+.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+        return `
+        <div class="photo-card">
+            <img src="${largeImageURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+                <p class="info-item">
+                    <b>Likes</b>${likes}
+                </p>
+                <p class="info-item">
+                    <b>Views</b>${views}
+                </p>
+                <p class="info-item">
+                    <b>Comments</b>${comments}
+                </p>
+                <p class="info-item">
+                    <b>Downloads</b>${downloads}
+                </p>
+            </div>
+        </div>
+        `
+    })
+    .join('')
+    return markup
+}
 
 function emptyArray() {
     Notify.failure('Sorry, there are no images matching your search query. Please try again.')
