d03b345869c7d4f644669d9ecf86009bfb2706ad
YevhenBez <byxgalter@bigmir.net>
2023-03-28 09:57:39 +0300

    Нашёл форму для поиска и кнопку для Поиска. Модуль 11
    Нашёл форму для поиска и кнопку для Поиска. Модуль 11

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import axios from 'axios';
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const searchForm = document.querySelector("#search-form");
+
+const btnSearch = searchForm.lastElementChild;
